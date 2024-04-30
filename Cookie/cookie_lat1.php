05c8432edf244a5232645fb4f9e1a2a071a98617
Hendro <hendrowunga073@gmail.com>
2024-05-01 00:25:37 +0700

    Add code

---
+++ Cookie/cookie_lat1.php
@@ -0,0 +1,14 @@
+<?php
+setcookie("cookie1", "Robertus Adi Nugroho");
+?>
+<html>
+
+<head>
+    <title>Membuat Cookie</title>
+</head>
+
+<body>
+    <h1>Selamat Cookie Sudah Terbentuk</h1>
+</body>
+
+</html>
---
+++ Cookie/cookie_lat2.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Membuat</title>
+</head>
+
+<body>
+    <?php
+    if (isset($_COOKIE['cookie1'])) {
+        echo ("<h1> Cookie ditemuan. Nilai Cookie adalah " . $_COOKIE['cookie1'] . "</h1>");
+    } else {
+        echo ("<h1> Maaf,Cookie tidak ditemukan</h1>");
+    }
+    ?>
+</body>
+
+
+</html>
