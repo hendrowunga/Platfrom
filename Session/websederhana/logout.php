45ef6cabd1d6c97f2b1212264bf969c023e5e19f
Hendro <hendrowunga073@gmail.com>
2024-05-02 06:05:03 +0700

    add code logout

---
+++ Session/websederhana/logout.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>E-Commerce</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="center">
+        <h1>Terima Kasih atas Kunjungannya</h1>
+        <p>
+            [ <a href="login.php">Login</a> ]
+        </p>
+    </div>
+    <?php
+    //session_destroy();
+    ?>
+</body>
+
+</html>
