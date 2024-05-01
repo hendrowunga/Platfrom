320efe9e14c133076354595ff28a4d84c75cc117
Hendro <hendrowunga073@gmail.com>
2024-05-02 06:11:11 +0700

    add code gagal login

---
+++ Session/websederhana/gagallogin.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>E-Commerce</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="center">
+        <h1>Maaf, Username atau Password Anda salah</h1>
+        <p>
+            [ <a href="login.php">Coba Kembali</a> ]
+        </p>
+    </div>
+</body>
+
+</html>
